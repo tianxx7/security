a36e4c57158814ddc1e9cb3e34becf37778824aa
tianxx7 <1729549607@qq.com>
2020-01-21 11:39:42 +0800

    自定义provider验证用户

--- src/main/java/com/txx/security/config/WebSecurityConfig.java
+++ src/main/java/com/txx/security/config/WebSecurityConfig.java
@@ -7,12 +7,16 @@ import com.txx.security.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Properties;
 
 /**
@@ -21,6 +25,10 @@ import java.util.Properties;
  */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationDetailsSource<HttpServletRequest,WebAuthenticationDetails> myWebAuthenticationDetailsSource;
+
 
     @Autowired
     public CustomerUserDetailsService userDetailsService;
@@ -35,14 +43,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/captcha.jpg").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login.html")
+                .formLogin()
+                .authenticationDetailsSource(myWebAuthenticationDetailsSource) // 应用AuthenticationDetailsSource
+                .loginPage("/login.html")
                 .loginProcessingUrl("/authentication/form");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
+        /*auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());*/
+        //应用自定义的provider
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        return new MyAuthenticationProvider(userDetailsService,new BCryptPasswordEncoder());
     }
 
     @Bean
